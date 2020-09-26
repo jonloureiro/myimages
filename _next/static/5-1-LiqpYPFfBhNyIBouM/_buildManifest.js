62fd82ef87311a2e97bfef2b05e1ac829cba53a4
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2020-09-26 19:24:47 -0400

    Updates

--- _next/static/5-1-LiqpYPFfBhNyIBouM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-48b251ac1325b7662b54.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-825e2af533a788ac2886.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
